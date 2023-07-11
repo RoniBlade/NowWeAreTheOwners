42cb7700ffa47abd8b52d1b9c8e1bca0d382b32f
RoniBlade <78499428+RoniBlade@users.noreply.github.com>
2023-07-11 17:06:37 +0300

    Add files via upload

---
+++ src/main/java/com/nowweareowner/nowweareowner/NowweareownerApplication.java
@@ -0,0 +1,13 @@
+package com.nowweareowner.nowweareowner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowweareownerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowweareownerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nowweareowner/nowweareowner/NowweareownerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowweareowner.nowweareowner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowweareownerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
