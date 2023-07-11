c896a6fc9d6874bd527dbc21fef8c09ce0e1115e
Adam Latyrov <adamlatyrov279@gmail.com>
2023-07-11 17:50:11 +0300

    Add User Service and User model

---
+++ src/main/java/com/nowweareowner/nowweareowner/impl/UserService.java
@@ -0,0 +1,30 @@
+package com.nowweareowner.nowweareowner.impl;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class UserService {
+
+    List<String> users;
+
+    public void addUser() {
+
+    }
+
+    public void addUsersToFile() {
+
+    }
+
+    public List<String> loadUsersFromFile() {
+        return null;
+    }
+
+    public void sort() {
+
+    }
+
+}
---
+++ src/main/java/com/nowweareowner/nowweareowner/models/User.java
@@ -0,0 +1,14 @@
+package com.nowweareowner.nowweareowner.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    String name;
+    String lastName;
+    String secondName;
+
+}
