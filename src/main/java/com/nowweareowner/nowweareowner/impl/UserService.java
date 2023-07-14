0a53e075ab57f3a78d3df9f6021dc9999739a53e
Adam Latyrov <adamlatyrov279@gmail.com>
2023-07-14 17:51:13 +0300

    adding the implementation of the methods upload data, upload data, add a user to a list, get a list of users. Add chat-like logic

--- src/main/java/com/nowweareowner/nowweareowner/impl/UserService.java
+++ src/main/java/com/nowweareowner/nowweareowner/impl/UserService.java
@@ -1,30 +1,75 @@
 package com.nowweareowner.nowweareowner.impl;
 
 
+import com.nowweareowner.nowweareowner.models.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 @AllArgsConstructor
 public class UserService {
 
     List<String> users;
 
-    public void addUser() {
 
+    public void addUser(String user) {
+        users.add(user);
     }
 
-    public void addUsersToFile() {
-
+    public void addUsersToFile(String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (String user : users) {
+                writer.write(user);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public List<String> loadUsersFromFile() {
-        return null;
+    public void loadUsersFromFile(String fileName) {
+        List<String> loadedUsers = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                loadedUsers.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        users.addAll(loadedUsers);
     }
 
     public void sort() {
+        Collections.sort(users);
+    }
 
+    public List<String> getUsers() {
+        return users;
+    }
+
+    public void userCommunication(User currentUser, User otherUser) {
+        Scanner scanner = new Scanner(System.in);
+        String message;
+
+        while (true) {
+            System.out.print(currentUser.getName() + ": ");
+            message = scanner.nextLine();
+
+            if (message.equalsIgnoreCase("exit")) {
+                System.out.println("Chat ended.");
+                break;
+            }
+
+            System.out.println(otherUser.getName() + ": " + message);
+        }
+
+        scanner.close();
     }
 
 }
